de07d5480e68c33e48337ac5f392d491c9df2d41
Hrushikesh kedar <151364132+rushikedar5@users.noreply.github.com>
2026-02-19 16:17:19 +0530

    Update app.js

--- ecomBackend/src/app.js
+++ ecomBackend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-frontend.vercel.app"
+    "https://e-aydkl2q3l-hrushikesh-kedars-projects.vercel.app/"
   ],
   credentials: true
 }));
