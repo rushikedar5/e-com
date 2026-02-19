c974bd5fe6bdb2eb847b1ce0c4a9e28003687153
Hrushikesh kedar <151364132+rushikedar5@users.noreply.github.com>
2026-02-19 16:18:22 +0530

    Update app.js

--- ecomBackend/src/app.js
+++ ecomBackend/src/app.js
@@ -8,13 +8,7 @@ import orderRoute from "./routes/order.route.js"
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://e-aydkl2q3l-hrushikesh-kedars-projects.vercel.app/"
-  ],
-  credentials: true
-}));
+app.use(cors());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1", productRoute);
 app.use("/api/v1", cartRoute);
